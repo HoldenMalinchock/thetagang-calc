dfc8823955d265ae390a7a224e136dfd5eaacbb4
Holden Malinchock <h.malinchock@verizon.net>
2023-04-25 23:57:27 -0400

    Fixing our go back button

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,16 +2,9 @@ export function Header() {
   return (
     <header class="h-[110px] w-full bg-cover bg-no-repeat relative bg-green-500">
       <div class="rainfall w-full h-full absolute" />
-      <nav class="w-11/12 h-24 max-w-5xl mx-auto flex items-center ">
+      <nav class="w-11/12 h-24 max-w-5xl mx-auto flex items-center relative">
         <a href="/">
-          {
-            /* <img
-            src="/logo.svg"
-            alt="Deno Logo"
-            class="h-14 w-14"
-          /> */
-          }
-          <h1>Theta Gang Calculator</h1>
+          <div>Theta Gang Calculator</div>
         </a>
       </nav>
     </header>
